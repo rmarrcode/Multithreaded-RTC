ef65e87ac0864645356fbd49314a30bb674d679e
marr0127 <marr0127@umn.edu>
2021-09-20 15:38:35 -0500

    thread creation

--- src/server/ConnectionInfo.cpp
+++
@@ -1,7 +0,0 @@
-#include "ConnectionInfo.h"
-
-ConnectionInfo::ConnectionInfo(int listening, int clientSocket, sockaddr_in client) {
-    _listening = listening;
-    _clientSocket = clientSocket;
-    _client = client;
-}
--- src/server/RTCServer.cpp
+++ src/server/RTCServer.cpp
@@ -8,91 +8,63 @@
 #include <string>
 #include <pthread.h> 
 
-#include "ConnectionInfo.h"
-
 using namespace std;
 
-void* handleConnection(void* infovdptr) {
-    ConnectionInfo info = *(ConnectionInfo *)infovdptr;
-
-    int listening = info.listening();
-    int clientSocket = info.clientSocket();
-    sockaddr_in client = info.client();
-
-    // start of func
+//TODO: record all available sockets and communicate to them 
+void* handleConnection(void* p_clientSocket) {
+    int clientSocket = *(int *)p_clientSocket;
     char host[NI_MAXHOST];      // Client's remote name
-    char service[NI_MAXSERV];   // Service (i.e. port) the client is connect on
-    
+    char service[NI_MAXSERV];   // port the client is connect on  
     memset(host, 0, NI_MAXHOST); 
     memset(service, 0, NI_MAXSERV);
 
-    if (getnameinfo((sockaddr*)&client, sizeof(client), host, NI_MAXHOST, service, NI_MAXSERV, 0) == 0) {
-        cout << host << " connected on port " << service << endl;
-    }
-    else {
-        inet_ntop(AF_INET, &client.sin_addr, host, NI_MAXHOST);
-        cout << host << " connected on port " << ntohs(client.sin_port) << endl;
-    }
-
-    // Close listening socket
-    close(listening);
-
-    // While loop: accept and echo message back to client
+    cout << "connected on " << clientSocket << endl;
+       
+    // accept and echo message back to client
     char buf[4096];
-
     while (true) {
         memset(buf, 0, 4096);
-
-        // Wait for client to send data
         int bytesReceived = recv(clientSocket, buf, 4096, 0);
         if (bytesReceived == -1) {
             cerr << "Error in recv(). Quitting" << endl;
             break;
         }
-
         if (bytesReceived == 0) {
             cout << "Client disconnected " << endl;
             break;
         }
-
         cout << string(buf, 0, bytesReceived) << endl;
-
         // Echo message back to client
         send(clientSocket, buf, bytesReceived + 1, 0);
     }
-
-    // Close the socket
     close(clientSocket);
-    //end of func
 }
 
+//TODO: handle failures
 int main(int argc, char *argv[]) {
-    // Create a socket
+    //Create a socket
     int serverSocket = socket(AF_INET, SOCK_STREAM, 0);
     if (serverSocket == -1) {
         cerr << "Can't create a socket! Quitting" << endl;
         return -1;
     }
-    // Bind the ip address and port to a socket
-    // TODO: separate socket class, find avail socket?
-    sockaddr_in hint;
-    hint.sin_family = AF_INET;
-    hint.sin_port = htons(54000);
-    inet_pton(AF_INET, "0.0.0.0", &hint.sin_addr);
-    
-    bind(serverSocket, (sockaddr*)&hint, sizeof(hint));
 
+    // Bind the ip address and port to a socket
+    sockaddr_in sockInfo;
+    sockInfo.sin_family = AF_INET;
+    sockInfo.sin_port = htons(54000);
+    inet_pton(AF_INET, "127.0.0.1", &sockInfo.sin_addr);
+    bind(serverSocket, (sockaddr*)&sockInfo, sizeof(sockInfo));
     listen(serverSocket, SOMAXCONN);
     
     // Wait for a connection
-    sockaddr_in client;
-    socklen_t clientSize = sizeof(client);
-    
+    // Create new thread and handle
     while(1) {
+        sockaddr_in client;
+        socklen_t clientSize = sizeof(client);
         int clientSocket = accept(serverSocket, (sockaddr*)&client, &clientSize);
-        ConnectionInfo info(serverSocket, clientSocket, client);
         pthread_t thread;
-        pthread_create(&thread, NULL, handleConnection, (void *)&info);
+        pthread_create(&thread, NULL, handleConnection, &clientSocket);
     }
     return 0;
 }
